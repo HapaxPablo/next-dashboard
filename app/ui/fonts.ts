dd4cacebed9e4c1a82f8846ac081786ad7aa83b7
Pavel <pahapsah11@gmail.com>
2024-02-26 15:46:17 +0700

    feat: add proj

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+/* файл для хранения шрифтов */
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
